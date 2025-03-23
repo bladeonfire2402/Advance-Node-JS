608e034aba3e96fcd710f26bf8874ec66e883483
yennhi-1107 <147493453+yennhi-1107@users.noreply.github.com>
2025-03-23 23:45:56 +0700

    hefw

--- src/router/newsRouter.js
+++ src/router/newsRouter.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 
 const newsRouter = Router()
 
+newsRouter.delete('/deleteNews',)//Delete tin tức
 newsRouter.get('/getNews',) //lấy tin tức
 newsRouter.post('/createNews',)//Tạo tin tức
 newsRouter.put('/updateNew',)//Update
-newsRouter.delete('/deleteNews',)//Delete tin tức
+
 
 export default newsRouter
