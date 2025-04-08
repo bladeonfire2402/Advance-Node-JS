0319bfed97e09edf0e691b1ff2787c6325c54802
bladeonfire2402 <liemp966@gmail.com>
2025-04-08 17:02:11 +0700

    mmm

--- src/router/testRouter.js
+++ src/router/testRouter.js
@@ -7,6 +7,8 @@ const testRouter= Router()
 testRouter.post('/login',authController.signIn) //http://localhost:4000/api/test/signIn
 testRouter.post('/register',authController.signUp) //http://localhost:4000/api/test/signUp
 testRouter.post('/sendEmail',authController.sendNotify) //http://localhost:4000/api/test/sendEmail
-
+testRouter.get('/test',(req,res)=>{
+    res.send("Xin chào ae")
+})
 
 export default testRouter
